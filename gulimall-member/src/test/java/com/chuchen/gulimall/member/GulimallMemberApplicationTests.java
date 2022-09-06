04d733298a18ea0c0f8711da6217d018159644e1
timechuchen <2665300871@qq.com>
2022-09-06 16:59:07 +0800

    项目初始化构建

---
+++ gulimall-member/src/test/java/com/chuchen/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuchen.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
