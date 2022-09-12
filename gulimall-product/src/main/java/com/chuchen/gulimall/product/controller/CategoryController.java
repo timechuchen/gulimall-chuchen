e212f18df5b8d988fb931d86675437bfb554b340
timechuchen <2665300871@qq.com>
2022-09-12 14:10:24 +0800

    完成了商品列表的展示与删除、拖拽，批量删除等操作

--- gulimall-product/src/main/java/com/chuchen/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/chuchen/gulimall/product/controller/CategoryController.java
@@ -52,7 +52,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId){
 		CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
@@ -73,6 +73,14 @@ public class CategoryController {
     //@RequiresPermissions("product:category:update")
     public R update(@RequestBody CategoryEntity category){
 		categoryService.updateById(category);
+
+        return R.ok();
+    }
+
+    @RequestMapping("/update/sort")
+    //@RequiresPermissions("product:category:update")
+    public R updateSort(@RequestBody CategoryEntity[] category){
+        categoryService.updateBatchById(Arrays.asList(category));
 
         return R.ok();
     }
